2e29f17bfa05782a34018c05e638d646ffedbe9b
Kirk Henderson <kirk733@me.com>
2017-10-14 15:04:51 -0600

    Init commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+//express and app
+const express = require('express');
+const app = express();
+var PORT = process.env.PORT || 5000;
+
+
+//route handler
+app.get('/', (req, res) => {
+    res.send({hi: 'there'});
+});
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
+
+
+
